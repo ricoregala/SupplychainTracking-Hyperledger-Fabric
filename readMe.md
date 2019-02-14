c37de6c2d856951c02c5206d90c583b05aed2ca2
ashtikuno <rico.regala@hotmail.com>
2019-02-14 15:52:33 +0800

    Edited readMe

---
+++ readMe.md
@@ -0,0 +1,16 @@
+# Hyperledger Fabric Supply Tracking Activity
+
+## Development Environment:
+#### Operating System: Ubuntu XFCE 18.04.2 (Xubuntu) LTS
+#### Hyperledger Fabric Version : 1.4
+#### Go Version : 1.10
+
+## PreRequisites:
+1. Download github.com/ashtikuno/prerequisites.sh
+2. Install Go https://golang.org/doc/install
+3. Install cURL (sudo apt install curl -y)
+4. curl -sSL https://github.com/hyperledger/fabric/blob/release-1.4/scripts/bootstrap.sh | bash -s 1.4.0
+5. This will download the needed binaries and then put it into the bin folder of the download path.
+6. Export the download path using: export PATH=<path to download location>/bin:$PATH
+
+##
--- supplytracking/startFabric.sh
+++ supplytracking/startFabric.sh
@@ -11,9 +11,9 @@ set -e
 export MSYS_NO_PATHCONV=1
 starttime=$(date +%s)
 LANGUAGE=${1:-"golang"}
-CC_SRC_PATH=github.com/fabcar/go
+CC_SRC_PATH=github.com/supplytracking/go
 if [ "$LANGUAGE" = "node" -o "$LANGUAGE" = "NODE" ]; then
-	CC_SRC_PATH=/opt/gopath/src/github.com/fabcar/node
+	CC_SRC_PATH=/opt/gopath/src/github.com/supplytracking/node
 fi
 
 # clean the keystore
@@ -27,10 +27,10 @@ cd ../basic-network
 # and prime the ledger with our 10 cars
 docker-compose -f ./docker-compose.yml up -d cli
 
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n fabcar -v 1.0 -p "$CC_SRC_PATH" -l "$LANGUAGE"
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n fabcar -l "$LANGUAGE" -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n supplytracking -v 1.0 -p "$CC_SRC_PATH" -l "$LANGUAGE"
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n supplytracking -l "$LANGUAGE" -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
 sleep 10
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n fabcar -c '{"function":"initLedger","Args":[""]}'
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n supplytracking -c '{"function":"initLedger","Args":[""]}'
 
 printf "\nTotal setup execution time : $(($(date +%s) - starttime)) secs ...\n\n\n"
 printf "Start by installing required packages run 'npm install'\n"
@@ -52,7 +52,7 @@ printf "The 'node query.js' may be run at anytime once the user has been registe
 # Copy packages from $GOPATH under chaincode.
 
 
-#peer chaincode install -n fabcar -v 1.1 -l "golang" -p "github.com/fabcar/go"
-#peer chaincode upgrade -n fabcar -v 1.1 -o orderer.example.com:7050 -C mychannel -l "golang" -p "github.com/fabcar/go" -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+#peer chaincode install -n supplytracking -v 1.1 -l "golang" -p "github.com/supplytracking/go"
+#peer chaincode upgrade -n supplytracking -v 1.1 -o orderer.example.com:7050 -C mychannel -l "golang" -p "github.com/supplytracking/go" -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
 
-#peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n fabcar -c '{"function":"queryAllCars","Args":[""]}'
+#peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n supplytracking -c '{"function":"queryAllCars","Args":[""]}'
